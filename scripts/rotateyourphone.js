363f480237c1b55dac4b3b6cf20c8cdf7b6f05e6
ovwvzu <55060619+ovwvzu@users.noreply.github.com>
2024-08-01 11:37:08 +0800

    Update rotateyourphone.js

--- scripts/rotateyourphone.js
+++ scripts/rotateyourphone.js
@@ -1,5 +1,5 @@
 var rotate_shown = false;
-document.body.innerHTML += '<div id="rf_rotate"></div>';
+document.body.innerHTML = '<div id="rf_rotate"></div>';
 _rf_checkratio();
 window.onresize = function(event) {
     _rf_checkratio();
@@ -21,7 +21,7 @@ function _rf_checkratio() {
 }
 
 function _rf_rotate(e) {
-    var rotateDiv = document.getElementById('rf_rotate');
+    const rotateDiv = document.getElementById('rf_rotate');
     if (e === 'show') {
         rotateDiv.style.width = '100%';
         rotateDiv.style.height = '100%';
@@ -32,11 +32,15 @@ function _rf_rotate(e) {
         rotateDiv.style.backgroundColor = 'black';
         rotateDiv.style.color = 'white';
         rotateDiv.style.display = 'flex';
+        rotateDiv.style.flexDirection = 'column';
         rotateDiv.style.alignItems = 'center';
         rotateDiv.style.justifyContent = 'center';
         rotateDiv.style.textAlign = 'center';
         rotateDiv.style.fontSize = '24px';
-        rotateDiv.innerHTML = 'Please rotate the screen to landscape mode';
+        rotateDiv.innerHTML = `
+            <img src="rotate.png" style="width: 300px; height: 300px; margin-bottom: 20px;" />
+            Please rotate the screen to landscape mode
+        `;
     } else {
         rotateDiv.style.removeProperty('width');
         rotateDiv.style.removeProperty('height');
@@ -47,6 +51,7 @@ function _rf_rotate(e) {
         rotateDiv.style.removeProperty('backgroundColor');
         rotateDiv.style.removeProperty('color');
         rotateDiv.style.removeProperty('display');
+        rotateDiv.style.removeProperty('flexDirection');
         rotateDiv.style.removeProperty('alignItems');
         rotateDiv.style.removeProperty('justifyContent');
         rotateDiv.style.removeProperty('textAlign');
