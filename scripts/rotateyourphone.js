7ea4f1ca769a82d52673f3c638969ae70620886a
ovwvzu <55060619+ovwvzu@users.noreply.github.com>
2024-08-01 11:48:31 +0800

    Update rotateyourphone.js

--- scripts/rotateyourphone.js
+++ scripts/rotateyourphone.js
@@ -1,5 +1,5 @@
 var rotate_shown = false;
-document.body.innerHTML = '<div id="rf_rotate"></div>';
+document.body.innerHTML += '<div id="rf_rotate"></div>';
 _rf_checkratio();
 window.onresize = function(event) {
     _rf_checkratio();
@@ -21,30 +21,36 @@ function _rf_checkratio() {
 }
 
 function _rf_rotate(e) {
-    const rotateDiv = document.getElementById('rf_rotate');
+    var rotateDiv = document.getElementById('rf_rotate');
     if (e === 'show') {
         rotateDiv.style.width = '100%';
         rotateDiv.style.height = '100%';
-        rotateDiv.style.position = 'fixed';
+        rotateDiv.style.position = 'absolute';
         rotateDiv.style.top = '0';
         rotateDiv.style.left = '0';
         rotateDiv.style.opacity = '0.8';
         rotateDiv.style.backgroundColor = 'black';
         rotateDiv.style.color = 'white';
         rotateDiv.style.display = 'flex';
-        rotateDiv.style.flexDirection = 'column';
         rotateDiv.style.alignItems = 'center';
         rotateDiv.style.justifyContent = 'center';
         rotateDiv.style.textAlign = 'center';
         rotateDiv.style.fontSize = '24px';
-        rotateDiv.innerHTML = `
-            <div style="display: flex; flex-direction: column; align-items: center;">
-                <img src="img/rotate.png" style="width: 300px; height: 300px; margin-bottom: 20px;" />
-                <span>Please rotate the screen to landscape mode</span>
-            </div>
-        `;
+        rotateDiv.innerHTML = 'Please rotate the screen to landscape mode';
     } else {
+        rotateDiv.style.removeProperty('width');
+        rotateDiv.style.removeProperty('height');
+        rotateDiv.style.removeProperty('position');
+        rotateDiv.style.removeProperty('top');
+        rotateDiv.style.removeProperty('left');
+        rotateDiv.style.removeProperty('opacity');
+        rotateDiv.style.removeProperty('backgroundColor');
+        rotateDiv.style.removeProperty('color');
+        rotateDiv.style.removeProperty('display');
+        rotateDiv.style.removeProperty('alignItems');
+        rotateDiv.style.removeProperty('justifyContent');
+        rotateDiv.style.removeProperty('textAlign');
+        rotateDiv.style.removeProperty('fontSize');
         rotateDiv.innerHTML = '';
-        rotateDiv.removeAttribute('style');
     }
 }
